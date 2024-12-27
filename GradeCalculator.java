b72907113723ee52db5e94b3e7f17b15120f35f4
maryannemomeh <maryannemomeh@gmail.com>
2024-12-26 21:17:31 -0500

    Update GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -2,19 +2,7 @@
 /*
 * GradeCalculator.java
 * Author: Maryanne Momeh
-* Statement of Academic Honesty:
-*
-* The following code represents my own work. I have neither
-* received nor given inappropriate assistance. I have not copied
-* or modified code from anywhere other than the authorized
-* sources. I recognize that any unauthorized sharing, assistance,
-* or plagiarism will be handled in accordance with both the
-* University of Georgia's Academic Honesty Policy and the
-* policies of this course. I recognize that my work is based on
-* an assignment created by the School of Computing
-* at the University of Georgia. Any publishing or posting
-* of source code at any time for this project is prohibited.
-*/
+
 import java.util.Scanner;
 
 public class GradeCalculator {
